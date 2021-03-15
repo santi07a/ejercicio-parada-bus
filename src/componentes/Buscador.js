bd7b4256bbf92283ac7c15f0e4f6569c3868d936
Malena de Juan <malena.dj4@gmail.com>
2021-03-15 20:14:40 +0100

    cambiado nombres variables y importado el useFetch

--- src/componentes/Buscador.js
+++ src/componentes/Buscador.js
@@ -1,17 +1,17 @@
 import PropTypes from "prop-types";
 
 const Buscador = props => {
-  const { busquedaParada, idParada } = props;
+  const { paradaBuscada, idParada } = props;
   return (
     <form>
-      <label htmlFor="num-parada">Parada nº: {busquedaParada} </label>
+      <label htmlFor="num-parada">Parada nº: {paradaBuscada} </label>
       <input type="number" id={`${idParada}`} />
       <button type="submit">Buscar</button>
     </form>);
 };
 
 Buscador.propTypes = {
-  busquedaParada: PropTypes.number.isRequired,
+  paradaBuscada: PropTypes.number.isRequired,
   idParada: PropTypes.string,
 };
 
