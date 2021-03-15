f44538bb9910b6a78cb920300324deae3027e00b
Malena de Juan <malena.dj4@gmail.com>
2021-03-15 22:39:00 +0100

    funcionamiento busqueda con objeto prueba

--- src/componentes/Formularios.js
+++ src/componentes/Formularios.js
@@ -1,21 +1,14 @@
 import Buscador from "./Buscador";
 import TiempoRestante from "./TiempoRestante";
-import PropTypes from "prop-types";
 
-const Formularios = props => {
-  const { datos, paradaBuscada, setParadaBuscada } = props;
+const Formularios = () => {
 
   return (
     <section className="forms">
-      <Buscador paradaBuscada={paradaBuscada} setParadaBuscada={setParadaBuscada} />
+      <Buscador />
       <TiempoRestante />
     </section>
   );
-};
-
-Formularios.propTypes = {
-  datos: PropTypes.array.isRequired,
-  paradaBuscada: PropTypes.string.isRequired,
 };
 
 export default Formularios;
