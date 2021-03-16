b13f82ec07ae2ce204bbb97f3526f35937487ba8
Malena de Juan <malena.dj4@gmail.com>
2021-03-16 20:16:28 +0100

    hacer que cambie el top con un setInterval

--- src/componentes/Display.js
+++ src/componentes/Display.js
@@ -1,11 +1,25 @@
 import PropTypes from "prop-types";
+import { useState } from "react";
 import { lineaType } from "../types/lineaType";
 import Lineas from "./Lineas";
 
 const Display = props => {
   const { parada } = props;
+  const [posicion, setPosicion] = useState(0);
+  if (parada.length > 1) {
+    setInterval(() => {
+      if (posicion !== -(30 * (parada.length - 1))) {
+        setPosicion(posicion - 30);
+      } else {
+        setPosicion(0);
+      }
+    }, 2000);
+  }
+
   return (
-    parada.map(linea => <Lineas key={linea.routeId} linea={linea}></Lineas >)
+    <div className="display">
+      {parada.map(linea => <Lineas key={linea.routeId} linea={linea} posicion={posicion}></Lineas >)}
+    </div>
   );
 };
 
