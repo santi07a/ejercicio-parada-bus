6bc81def5b87f66c1d57254b8dca55cc74838b21
Santi Arnau <santi07a@gmail.com>
2021-03-16 18:05:30 +0100

    creado contextos para agilizar el uso de sus variables

---
+++ src/Contexts/GeneralContext.js
@@ -0,0 +1,6 @@
+import { createContext } from "react";
+
+const GeneralContext = createContext();
+GeneralContext.displayName = "GeneralContext";
+
+export default GeneralContext;
---
+++ src/Contexts/OcultarContext.js
@@ -0,0 +1,6 @@
+import { createContext } from "react";
+
+const OcultarContext = createContext();
+OcultarContext.displayName = "OcultarContext";
+
+export default OcultarContext;
