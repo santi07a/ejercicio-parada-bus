925df5c055531450acfbc5678f630ffd79220e76
Santi Arnau <santi07a@gmail.com>
2021-03-19 18:40:56 +0100

    agrego setOcultarFrase(true) despues de submit

--- src/App.js
+++ src/App.js
@@ -28,11 +28,11 @@ function App() {
   const { data } = paradaApi;
   const [ocultarFrase, setOcultarFrase] = useState(true);
   const [tiempo, setTiempo] = useState(0);
-  const { ibus, ibus: [{ line, destination, routeId, "t-in-min": tiempoEnMinutos }] } = data;
+  const { ibus, ibus: [{ line, "t-in-min": tiempoEnMinutos }] } = data;
   const paradaPrueba = data.ibus;
 
   return (
-    <GeneralContext.Provider value={{ ibus, routeId, paradaPrueba, ocultarFrase, tiempo, paradaBuscada, setParadaBuscada, setLinea, setOcultarFrase, setTiempo }}>
+    <GeneralContext.Provider value={{ ibus, paradaPrueba, ocultarFrase, tiempo, paradaBuscada, setParadaBuscada, setLinea, setOcultarFrase, setTiempo }}>
       <div className="contenedor">
         <header className="cabecera">
           <Titular numeroParada={paradaBuscada} />
