8669ef06e587ed0851fcb63fb2709455cda3b05b
Santi Arnau <santi07a@gmail.com>
2021-03-21 18:46:01 +0100

    agregado map de Linea.js

--- src/Paginas/Linea.js
+++ src/Paginas/Linea.js
@@ -1,38 +1,39 @@
-import { useContext } from "react";
-import { Route } from "react-router";
+import { useContext, useEffect } from "react";
+import { Route, useParams } from "react-router";
 import GeneralContext from "../Contexts/GeneralContext";
 import NotFound from "./NotFound";
+import useFetch from "../hooks/useFetch";
+import ParadaContext from "../Contexts/ParadaContext";
 
 const Linea = () => {
-  const { parada, existeParada } = useContext(GeneralContext);
-
-  if (!parada || !existeParada) {
-
-    return <Route path="*" exact>
-      <NotFound />
-    </Route>;
-  } else {
-    const bus = parada.data.ibus;
-    const inicioYDestino = existeParada.features[0].properties.DESC_PARADA;
-    const nombreParada = existeParada.features[0].properties.NOM_PARADA;
-    return (
-
-      <div className="contenedor">
+  let { id } = useParams();
+  const { datos: datosParadasLinea, pedirDatos: pedirParadasLinea } = useFetch();
+  const urlId = `https://api.tmb.cat/v1/transit/linies/bus/${id}/parades${process.env.REACT_APP_API_KEY} `;
+  console.log(id);
+  useEffect(() => {
+    pedirParadasLinea(urlId);
+  }, [urlId, pedirParadasLinea]);
+  console.log(datosParadasLinea);
+  return (
+    datosParadasLinea ?
+      <>
         <header className="cabecera">
-          <h2>Bus {bus.line} - {inicioYDestino}</h2>
-          <h3>{nombreParada}</h3>
-          <a href="./Parada">Volver a la portada</a>
+          <h2>{/* Bus {id} - Hospital Clínic / Polígon Zona Franca */}</h2>
+          <h3>{/* Polígon Zona Franca -{">"} Hospital Clínic */}</h3>
+          <a href="/*">{/* Volver a la portada */}</a>
         </header>
         <section>
-          {/*   <ul className="grafico-paradas">
-            {existeParada.features.map(
-              parada => <li className="parada">Parada nº {parada}: {nombreParada} (<a href="/">ver mapa</a>)</li>
-            )}
-          </ul> */}
+          <ul className="grafico-paradas">
+            {datosParadasLinea.features.map(parada =>
+              <li key={parada.id} className="parada">Parada nº {parada.properties.CODI_PARADA}: {parada.properties.NOM_PARADA}
+                &nbsp;(<a href={`http://maps.google.com/maps?z=19&t=m&q=loc:
+                  ${parada.geometry.coordinates[1]}+${parada.geometry.coordinates[0]}`}>ver mapa
+                </a>)
+              </li>)}
+          </ul>
         </section>
-      </div>
-    );
-  };
+      </> : <p>cargando</p>
+  );
 };
 
 export default Linea;
